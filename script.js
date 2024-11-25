95d0c86eabafbc382c1bff7e5a1a9fd63684e7c8
JoshualBalzani <joshbalzani@gmail.com>
2024-11-25 15:10:46 +0100

    Update script.js

--- script.js
+++ script.js
@@ -131,7 +131,7 @@ class ShoppingCart {
 
         const message = this.generateWhatsAppMessage();
         const encodedMessage = encodeURIComponent(message);
-        const whatsappNumber = '3293891883'; // Replace with your number
+        const whatsappNumber = '+393924231439'; 
         const whatsappUrl = `https://wa.me/${whatsappNumber}?text=${encodedMessage}`;
         window.open(whatsappUrl, '_blank');
     }
